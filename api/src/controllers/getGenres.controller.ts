ca4a183c886ddeff2fc00d45d18c2bbd5492233f
LucasAEscudero <lucasescudero5629@gmail.com>
2024-03-11 00:20:28 -0300

    refactor genres and platforms controllers

--- api/src/controllers/getGenres.controller.ts
+++ api/src/controllers/getGenres.controller.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
 import { FetchError } from "../utils/errors";
 
+interface genresApiType {
+  id: string;
+  name: string;
+}
+
 export default async function getGenresController() {
-  let genres: string[] = [];
-  const genresApi = (await axios.get(`/genres?key=${process.env.API_KEY}`))
-    .data;
+  let genres: genresApiType[] = [];
+  const genresApi = (
+    await axios.get(`/genres?key=${process.env.API_KEY}&ordering=id`)
+  ).data;
 
   if (!genresApi) throw new FetchError("Error to fetch genres data", 404);
 
-  genresApi.results.forEach((genre: { name: string }) => {
-    genres.push(genre.name);
+  genresApi.results.forEach((genre: genresApiType) => {
+    genres.push({ id: genre.id, name: genre.name });
   });
 
   return genres;
--- api/src/controllers/getPlatforms.controller.ts
+++ api/src/controllers/getPlatforms.controller.ts
@@ -1,26 +1,31 @@
 import axios from "axios";
 import { FetchError } from "../utils/errors";
 
+interface platformsApiType {
+  id: string;
+  name: string;
+}
+
 export default async function getPlatformsController() {
-  let platforms: string[] = [];
+  let platforms: platformsApiType[] = [];
 
   const platformsApi1 = (
-    await axios.get(`/platforms?key=${process.env.API_KEY}&page=1`)
+    await axios.get(`/platforms?key=${process.env.API_KEY}&page=1&ordering=id`)
   ).data;
 
   const platformsApi2 = (
-    await axios.get(`/platforms?key=${process.env.API_KEY}&page=2`)
+    await axios.get(`/platforms?key=${process.env.API_KEY}&page=2&ordering=id`)
   ).data;
 
   if (!platformsApi1 || !platformsApi2)
     throw new FetchError("Error to fetch platforms data", 404);
 
-  platformsApi1.results.forEach((platform: { name: string }) =>
-    platforms.push(platform.name)
+  platformsApi1.results.forEach((platform: platformsApiType) =>
+    platforms.push({ id: platform.id, name: platform.name })
   );
 
-  platformsApi2.results.forEach((platform: { name: string }) =>
-    platforms.push(platform.name)
+  platformsApi2.results.forEach((platform: platformsApiType) =>
+    platforms.push({ id: platform.id, name: platform.name })
   );
 
   return platforms;
